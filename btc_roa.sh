64bfebdd0f64e5ce2cee6dc7610d4ea0afa8fdb5
thelueth <45825560+thelueth@users.noreply.github.com>
2019-06-21 15:49:44 +0200

    Update btc_roa.sh

--- btc_roa.sh
+++ btc_roa.sh
@@ -1,6 +1,10 @@
-curl https://api.coindesk.com/v1/bpi/currentprice.json -o btc_price.json
+#!/bin/bash
 
+#this accesses the coinbase API to request the current bitcoin price and creates file btc_price.json used in the python script
+curl https://api.coindesk.com/v1/bpi/currentprice.json -o btc_price.json 
 
-python /home/jonas/Desktop/btc_return.py 
+#runs python script
+python btc_roa.py 
 
+#removes the JSON file. no unnecessary files
 rm btc_price.json
