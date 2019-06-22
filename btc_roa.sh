a292ed08564b5b3ee68ef0ec304f15c167cd4abe
thelueth <45825560+thelueth@users.noreply.github.com>
2019-06-22 17:29:50 +0200

    btc_roa.sh
    added silent parameter to curl.

--- btc_roa.sh
+++ btc_roa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #this accesses the coinbase API to request the current bitcoin price and creates file btc_price.json used in the python script
-curl https://api.coindesk.com/v1/bpi/currentprice.json -o btc_price.json 
+curl https://api.coindesk.com/v1/bpi/currentprice.json -sS -o btc_price.json #-sS hides the progress of CURL
 
 #runs python script
 python btc_roa.py 
